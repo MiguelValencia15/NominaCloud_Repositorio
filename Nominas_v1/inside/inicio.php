ac9062eb7973976289ed1fc464dd084bf5192de4
MiguelValencia15 <148389474+MiguelValencia15@users.noreply.github.com>
2023-11-04 12:43:44 -0600

    Add files via upload
    Version 1 avance en interfaces

---
+++ Nominas_v1/inside/inicio.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NóminaCloud - Menu</title>
+    <link rel="stylesheet" href="../css/inicio.css">
+</head>
+<body>
+    <header>
+        <div class="dropdown">
+            <h1 class="nom">NóminaCloud</h1>
+        </div>
+        <a href="../index.php" class="logout-button">Cerrar sesión</a>
+    </header>
+    
+    <div class="container">
+        <div class="header-content">
+            <h2>Empresas</h2>
+            <a href="empresas.php" class="boton">
+                <img src="../view/add.jpg" alt="Agregar">
+            </a>
+            <a href="" class="boton1">
+                <img src="../view/ayu.jpg" alt="Ayuda">
+            </a>
+            <a href="nominas.php" class="boton2">
+                <img src="../view/logo2R.png" alt="Nómina">
+            </a>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre de la empresa</th>
+                    <th>RFC de la empresa</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Nombre de la Empresa 1</td>
+                    <td>RFC Empresa 1</td>
+                </tr>
+                <tr>
+                    <td>Nombre de la Empresa 2</td>
+                    <td>RFC Empresa 2</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
